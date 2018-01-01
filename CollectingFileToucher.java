99a0bae16656b3bcbd6cb115237d8880cc22e87d
Michael Carey <kb7ppb@hotmail.com>
2017-12-31 23:19:44 -0700

    BYU-Idaho study course work completed in 2006

---
+++ CollectingFileToucher.java
@@ -0,0 +1,57 @@
+import java.io.File;
+
+import java.io.FileFilter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Puts the given files into an ArrayList.
+ */
+public class CollectingFileToucher
+   extends FileToucher
+{
+   /**
+    * Holds the files being touched.
+    */
+   private Collection<File> mFiles;
+
+   /**
+    * Calls the superclass constructor and creates an empty list.
+    */
+   public CollectingFileToucher(String[] args)
+   {
+      super(args);
+      mFiles = new ArrayList<File>();
+      mFileFilter = new JavaSoftFileToucher(args);
+/*
+         {
+            public boolean accept(File inFile)
+            {
+               return true;
+            }
+         };
+*/
+   }
+
+   public Collection<File> getFiles()
+   {
+      return mFiles;
+   }
+
+   /**
+    * Adds a given file to the list.
+    */
+   public void touchOne(File inFile)
+   {
+      mFiles.add(inFile);
+   }
+
+   /**
+    * Outputs the list.
+    */
+   public void report(File inFile)
+   {
+      System.out.println("Collected " + mFiles + " in " + inFile);
+   }
+}
